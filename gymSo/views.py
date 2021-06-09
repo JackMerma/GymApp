282f3e8b7bea9ec5f28778500b8a39ac2ed49ad5
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-09 14:10:38 -0500

    Creacion del objeto Destionation y se pasa al renderizar el index.html un objeto tipo Destination

--- gymSo/views.py
+++ gymSo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html", {})
+
+    dest1 = Destination() 
+    return render(request, "index.html", {'dest1', dest1})
