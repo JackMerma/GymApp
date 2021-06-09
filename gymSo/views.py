4f8b8225cf71fc79aa9a3a105cd227d1e406fdbf
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-09 14:39:08 -0500

    creacion de 2 objetos dipo Destionation(data introducida) y llamado a los valores de estos objetos en el index.html

--- gymSo/views.py
+++ gymSo/views.py
@@ -8,5 +8,18 @@ def home(request):
     dest1 = Destination() 
     dest1.name="Yoga"
     dest1.trainer="Bella"
-    dest1.price="55"
-    return render(request, "index.html", {'dest1': dest1})
+    dest1.price=55
+    dest1.desc=""
+
+    dest2 = Destination() 
+    dest2.name="Areobic"
+    dest2.trainer="Mary"
+    dest2.price=66
+    dest2.desc=""
+
+    dest3 = Destination() 
+    dest3.name="Cardio"
+    dest3.trainer="Cathe"
+    dest3.price=75
+    dest3.desc=""
+    return render(request, "index.html", {'dest1': dest1, 'dest2':dest2, 'dest3':dest3})
