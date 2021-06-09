9815f4134919db4db904c8f0c2b665efea7771ac
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-09 14:45:44 -0500

    modificacion del atributo img para cada objeto y creacion de un arreglo de objetos y envio de este mismo

--- gymSo/views.py
+++ gymSo/views.py
@@ -10,16 +10,22 @@ def home(request):
     dest1.trainer="Bella"
     dest1.price=55
     dest1.desc=""
+    dest1.img='yoga-class.jpg'
 
     dest2 = Destination() 
     dest2.name="Areobic"
     dest2.trainer="Mary"
     dest2.price=66
     dest2.desc=""
+    dest1.img='crossfit-class.jpg'
 
     dest3 = Destination() 
     dest3.name="Cardio"
     dest3.trainer="Cathe"
     dest3.price=75
     dest3.desc=""
-    return render(request, "index.html", {'dest1': dest1, 'dest2':dest2, 'dest3':dest3})
+    dest3.img='cardio-class.jpg'
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, "index.html", {'dests': dests})
