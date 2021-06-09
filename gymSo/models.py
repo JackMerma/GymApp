eb60130afbfd512b7db3e1bbd162f91d33f0fbf4
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-09 16:18:39 -0500

    uso de models en los atributos de Destination

--- gymSo/models.py
+++ gymSo/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id: int 
-    name: str
-    img: str 
-    desc: str
-    price: int 
-    trainer: str 
+class Destination(models.Model):
+    name = models.CharField(max_length=100) 
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    trainer = model.CharField(max_length=100)
 
