4d4f0a3231f64b0d1c8f02730dcc017f1ffb95b9
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-09 17:16:15 -0500

    eliminacion de los objetos creados en views y trabajando con objetos directamente de Destinations y en el index implementarlo

--- gymSo/views.py
+++ gymSo/views.py
@@ -5,27 +5,6 @@ from .models import Destination
 # Create your views here.
 def home(request):
 
-    dest1 = Destination() 
-    dest1.name="Yoga"
-    dest1.trainer="Bella"
-    dest1.price=55
-    dest1.desc=""
-    dest1.img='yoga-class.jpg'
-
-    dest2 = Destination() 
-    dest2.name="Areobic"
-    dest2.trainer="Mary"
-    dest2.price=66
-    dest2.desc=""
-    dest2.img='crossfit-class.jpg'
-
-    dest3 = Destination() 
-    dest3.name="Cardio"
-    dest3.trainer="Cathe"
-    dest3.price=75
-    dest3.desc=""
-    dest3.img='cardio-class.jpg'
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request, "index.html", {'dests': dests})
